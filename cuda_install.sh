1cdf458e1dd52bad16188c46b30146e4d30d2a60
Alex Melkonyan <sasha.melkonyan@gmail.com>
2017-06-17 12:34:09 +0200

    Add script for installing cuda

---
+++ cuda_install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+wget https://www.dropbox.com/s/9ljw0v3ycsqavjr/cudnn-8.0-linux-x64-v5.1.tgz
+tar -xvzf cudnn-8.0-linux-x64-v5.1.tgz
+sudo cp cuda/lib64/libcudnn* /usr/local/cuda-8.0/lib64/
+
+sudo cp cuda/include/cudnn.h /usr/local/cuda-8.0/include
